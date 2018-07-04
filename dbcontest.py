1a7b4271ce5392f0773c59e316c924d05de25c34
Munir93 <37178013+Munir93@users.noreply.github.com>
2018-07-04 11:18:48 +0100

    Update dbcontest.py

--- dbcontest.py
+++ dbcontest.py
@@ -1,6 +1,6 @@
 import pymysql as sql
 
-conn = sql.connect(host='35.189.118.151',db='test', user='root', password='killbill4')
+conn = sql.connect(host='35.234.159.225',db='test', user='root', password='killbill4')
 cur= conn.cursor()
 cur.execute('CREATE TABLE logs(remote_add varchar(100), time_local varchar(100), request varchar(100), status varchar(100), body_bytes_sent varchar(100), http_referer varchar(100), final_browser varchar(100), os varchar(100))' )
 cur.close()
