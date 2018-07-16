312cf9fbde9bcf3d9c7f346f55eef7444932106e
Munir93 <37178013+Munir93@users.noreply.github.com>
2018-07-16 22:30:06 +0100

    Add files via upload

--- twitter-sentiment.py
+++ twitter-sentiment.py
@@ -6,6 +6,7 @@ from google.cloud import bigquery
 #dataset_ref = client.dataset(dataset_id, project=config.PROJECT_ID)
 
 client = bigquery.Client()
+dataset_ref = client.dataset('test_dataset', 'warm-airline-207713')
 
 datasets = client.list_datasets()
 list_of_datasets = []
@@ -14,3 +15,15 @@ for dataset in datasets:
     list_of_datasets.append(dataset.dataset_id)
 
 print(list_of_datasets)
+
+job_config = bigquery.LoadJobConfig()
+
+job_config.autodetect = True
+
+uri = 'gs://dod-mwja-project1/Tweets2018_07_12_17_21_16.csv'
+table_name = 'pleasework'
+
+load_job = client.load_table_from_uri(
+        source_uris=uri,destination=dataset_ref.table(table_name),job_config=job_config)
+
+
