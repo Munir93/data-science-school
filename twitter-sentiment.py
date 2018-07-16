dc7d04acb9962339494e77374622cbf5fd3b68e4
Munir93 <37178013+Munir93@users.noreply.github.com>
2018-07-16 22:13:17 +0100

    Add files via upload

---
+++ twitter-sentiment.py
@@ -0,0 +1,10 @@
+from googleapiclient import discovery
+from google.cloud import bigquery
+
+#client = bigquery.Client.from_service_account_json(json_credentials_path=config.KEY_PATH,project=config.PROJECT_ID)
+dataset_id = config.DATASET
+#dataset_ref = client.dataset(dataset_id, project=config.PROJECT_ID)
+
+
+client1 = bigquery.Client()
+client1.list_dataset_tables(dataset_id)
