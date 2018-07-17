4c8e2c38d6178f12dd73c1b3d2c245db5dcabe6b
Munir93 <37178013+Munir93@users.noreply.github.com>
2018-07-17 13:19:08 +0100

    Delete twitter-config.py

--- DataMigrationTwitterETL-Code-ONLY/twitter-config.py
+++
@@ -1,9 +0,0 @@
-TRACK_TERMS = ["itscominghome", "englandworldcup", "Englandworldcup"]
-
-TWITTER_APP_KEY = 'KPVEb25geZ1WUxMrJz3flS6je'
-TWITTER_APP_SECRET = 'BcjmYWEJmDuU81gwW3Hs64mnzH4I67EzgS0H5FfuIs7VFjkXtH'
-TWITTER_KEY = '1014592058215059456-Z1pGlZHdMvx8vsszkDWpsEdTCjfz4o'
-TWITTER_SECRET = 'Zsw17mh9OiXDfaf993njgHhGZHxYTHrrD9Ae8fgqEc1mq'
-
-
-# Here we have generated acess tokens to allow us to connect to the tweitter api
